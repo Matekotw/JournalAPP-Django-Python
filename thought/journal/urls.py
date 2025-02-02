1f8e8f39b2a89e4b6633514c2a5b9c9341b7ec9b
Mateusz Kotwicki <160995475+Matekotw@users.noreply.github.com>
2025-02-02 17:46:28 +0100

    Add files via upload

---
+++ thought/journal/urls.py
@@ -0,0 +1,51 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path('', views.homepage, name=""),
+
+    path('register', views.register, name="register"),
+
+    path('my-login', views.my_login, name="my-login"),
+
+    path('dashboard', views.dashboard, name="dashboard"),
+
+    path('user-logout', views.user_logout, name="user-logout"),
+
+    path('create-thought', views.create_thought, name="create-thought"),
+
+    path('my-thoughts', views.my_thoughts, name="my-thoughts"),
+
+
+    path('update-thought/<str:pk>', views.update_thought, name="update-thought"),
+
+
+    path('delete-thought/<str:pk>', views.delete_thought, name="delete-thought"),
+
+    path('profile-management', views.profile_management, name="profile-management"),
+
+    path('delete-account', views.delete_account, name="delete-account"),
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
